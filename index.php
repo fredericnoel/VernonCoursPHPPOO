7f8c15844a72cd0cfece461bf027b6a12d080cab
Frédéric NOËL <contact@fredericnoel.com>
2022-02-14 11:08:50 +0100

    Installation librairie php-timer

--- index.php
+++ index.php
@@ -2,14 +2,43 @@
 date_default_timezone_set('Europe/Paris');
 ini_set('display_errors', '1');
 
-spl_autoload_register(function ($className) {
+include './vendor/autoload.php';
+
+
+/*spl_autoload_register(function ($className) {
     include './classes/' . $className . '.php';
 });
+*/
 
-/*$message = new Machin();
+use SebastianBergmann\Timer\Timer;
+$timerFor = new Timer();
+$timerWhile = new Timer();
 
-@$message->bonjour("Michel");
-echo "<hr />";
+$timerFor->start();
+
+for ($i = 0 ; $i < 10000 ; $i++) {}
+
+$resultatFor = $timerFor->stop();
+var_dump($resultatFor->asMicroseconds());
+
+$timerWhile->start();
+echo "<br />";
+
+$i = 0;
+while ($i < 10000) {
+    $i++;
+}
+
+$resultatWhile = $timerWhile->stop();
+var_dump($resultatWhile->asMicroseconds());
+
+//var_dump($timer);
+//$value = $timer->stop();
+//$time = $timer->stop();
+/*$message = new Machin();*/
+//var_dump($value->asMilliseconds());
+//$message->bonjour("Michel");
+/*echo "<hr />";
 
 $homme = new Test();
 
@@ -17,7 +46,7 @@ $homme->setAge(42);
 
 echo $homme->age;
 
-var_dump($homme);*/
+var_dump($homme);
 
 include './classes/NamespaceFormel.php';
 
@@ -27,9 +56,11 @@ machin();
 use function \bonjour as salut;
 salut();
 
-/*$utilisateur = new \Exemple\User();*/
+$utilisateur = new \Exemple\User();
 
 use \Exemple\User;
 $utilisateur = new User();
 
 var_dump($utilisateur);
+*/
+
